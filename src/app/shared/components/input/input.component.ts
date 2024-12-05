7f988cc587001398da325f49f2188764baff87f8
Emma <mellgrenemma0@gmail.com>
2024-12-05 22:22:36 +0100

    Refactored code for simplicity

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -3,43 +3,42 @@ import { Component, Input, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
-  selector: 'app-input',
-  imports: [NgIf, NgClass],
-  templateUrl: './input.component.html',
-  styles: '',
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => InputComponent),
-      multi: true,
-    },
-  ],
+	selector: 'app-input',
+	imports: [NgClass],
+	templateUrl: './input.component.html',
+	styles: '',
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => InputComponent),
+			multi: true,
+		},
+	],
 })
 export class InputComponent implements ControlValueAccessor {
-  value: string = '';
-  @Input() placeholder: string = '';
-  @Input() type: string = 'text';
-  @Input() label: string = '';
-  @Input() error: string = '';
+	value: string = '';
+	@Input() placeholder: string = '';
+	@Input() type: string = 'text';
+	@Input() errorMessage: string = '';
 
-  onChange = (value: string) => {};
-  onTouched = () => {};
+	onChange = (value: string) => {};
+	onTouched = () => {};
 
-  writeValue(value: string): void {
-    this.value = value;
-  }
+	writeValue(value: string): void {
+		this.value = value;
+	}
 
-  registerOnChange(fn: (value: string) => void): void {
-    this.onChange = fn;
-  }
+	registerOnChange(fn: (value: string) => void): void {
+		this.onChange = fn;
+	}
 
-  registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
+	registerOnTouched(fn: () => void): void {
+		this.onTouched = fn;
+	}
 
-  onInput(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    this.value = input.value;
-    this.onChange(this.value);
-  }
+	onInput(e: Event): void {
+		const input = e.target as HTMLInputElement;
+		this.value = input.value;
+		this.onChange(this.value);
+	}
 }
